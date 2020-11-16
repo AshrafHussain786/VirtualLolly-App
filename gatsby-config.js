6afe9407ca555ddb3e5a1ff7200117ec7f67f620
Haris Aqeel <harisaqeel.2001@gmail.com>
2020-11-16 11:57:36 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,18 @@
-module.exports = {
-    plugins: [
 
-    ]
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain remote schema Query type
+        typeName: "Lollies",
+        // This is the field under which it's accessible
+        fieldName: "LOLLIES",
+        // URL to query from
+        url: "/.netlify/functions/createLolly/",
+      },
+    },
+
+    
+  ],
 }
