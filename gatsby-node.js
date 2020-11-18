68ce9ad5857af09849524df02489211e293eb81d
Haris Aqeel <harisaqeel.2001@gmail.com>
2020-11-18 21:12:53 +0500

    ADDED

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,29 +1,29 @@
-const path = require(`path`)
-exports.createPages = async ({ actions, graphql }) => {
-  console.log(graphql)
-  const result = await graphql(`
-  query MyQuery{
-    LOLLIES{
-    getAllLollies{
-      recipientName
-      senderName
-      message
-      topColor
-      mediumColor
-      bottomColor
-      path
-  }}}
-  `)
+// const path = require(`path`)
+// exports.createPages = async ({ actions, graphql }) => {
+//   console.log(graphql)
+//   const result = await graphql(`
+//   query MyQuery{
+//     LOLLIES{
+//     getAllLollies{
+//       recipientName
+//       senderName
+//       message
+//       topColor
+//       mediumColor
+//       bottomColor
+//       path
+//   }}}
+//   `)
 
 
-  result.data.LOLLIES.getAllLollies.map(async (indLolly) => {
-    console.log(indLolly)
-    await actions.createPage({
-      path: `lolly/${indLolly.path}`,
-      component: path.resolve(`./src/template/lollyPage.jsx`),
-      context: {
-        lollyPath: indLolly.path,
-      },
-    })
-  })
-}
+//   result.data.LOLLIES.getAllLollies.map(async (indLolly) => {
+//     console.log(indLolly)
+//     await actions.createPage({
+//       path: `lolly/${indLolly.path}`,
+//       component: path.resolve(`./src/template/lollyPage.jsx`),
+//       context: {
+//         lollyPath: indLolly.path,
+//       },
+//     })
+//   })
+// }
